fcb1f9c212337e207af3e79ad0d06b921a637d69
Ashwuni Kumar <a255kuma@uwaterloo.ca>
2020-06-26 09:42:16 +0000

    similar tables

--- lib/show-most-similar-titles.rb
+++ lib/show-most-similar-titles.rb
@@ -7,7 +7,7 @@ def format_similar_message source_book_title, most_similar_title, similarity_sco
     " also liked #{most_similar_title}." +
     " (Score = %0.3f)" % [similarity_score]
 end
-    
+
 # TODO: Insert your code here to produce the expected output
 # found in expected-output/show-most-similar-titles.txt .
 #
@@ -17,4 +17,29 @@ end
 # To output the books in the correct order, you'll 
 # need process them in order of their book_ids (ascending).
     
+#def show_most_similar_titles
+    all_of_the_books=Book.get_ids().sort
+    
+    #puts books
+    
+    for i in 1..all_of_the_books.count #all the book ids
+        max_score= -100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000#similarity(all_of_the_books[0], all_of_the_books[1])
+        #puts i
+        for j in 1..all_of_the_books.count#test against each book
+            if all_of_the_books[i]==all_of_the_books[j]
+                next j
+            end
+            
+            similarity=similarity(all_of_the_books[i-1], all_of_the_books[j-1])
+
+            if similarity>max_score #assuming only need to suggest one of the most similar books
+                max_score=similarity
+                book2_id = all_of_the_books[j-1]
+            end
+           
+        end
+        p= format_similar_message(Book.get_title(all_of_the_books[i-1]), Book.get_title(book2_id), max_score)
+        puts p
+    end
+#end
 
